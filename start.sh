e8af65a807c9c3351778ec0d8ecdb20223453b79
Алиса Кузьменко <alisa.kas2006@gmail.com>
2025-12-01 22:50:45 +0300

    readme

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Запуск приложения 'Мои рецепты'..."
+echo ""
+cd backend
+xdg-open http://localhost:5000 2>/dev/null || open http://localhost:5000 2>/dev/null || start http://localhost:5000 2>/dev/null
+python3 app.py
+
